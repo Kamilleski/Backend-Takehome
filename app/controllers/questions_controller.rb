793dba0b705bfce0b7b27dd7e5e4b950dd07db92
Kamille Delgardo <kamilledelgardo@gmail.com>
2016-05-12 13:52:37 -0700

    Two hours up

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,9 @@
 class QuestionsController < ApplicationController
   def index
     @num_questions
-  end
-  def show
     @noun_questions = Question.where(strand_id: 1)
     @verb_questions = Question.where(strand_id: 2)
     # The following creates an array where each element alternates between strand 1 and 2
     @questions = @noun_questions.zip(@verb_questions).flatten.compact
-
-    @num_questions = @num_questions
-    @num_questions.times do |q|
+  end
 end
